9a831994623dc4d5137755c6dd1bf7969a2295a5
Jeffrey McCullen <McCullen_J@lynchburg.edu>
2016-03-23 22:52:40 -0400

    Delete extra functions and comments and add checks for invalid input.

--- array.lisp
+++ array.lisp
@@ -12,18 +12,26 @@
 
 (defun my-nth1 (list index)
   "Returns the index-th element of list starting at 1"
+  ;; If input is invalid, return nil
+  (cond ((null list) nil)
+        ((< index 1) nil)
   ;; If the index is 1, return the first element
-  (cond ((= index 1) (first list))
+        ((= index 1) (first list))
 	;((null list) 'error)
   ;; Else, return the nth element of the rest of the list
 	(t (my-nth1 (rest list) (- index 1)))))
 
 (defun my-nth (list index)
   "Returns the index-th element of list starting at 0"
-  ;; If the index is 1, return the first element
-  (cond ((= index 0) (first list))
+  (cond 
+  ;; If list is invalid, return 0
+    ((null list) nil)
+  ;; If the index is less than 0, return nil.
+    ((< index 0) nil)
+  ;; If the index is 0, return the first element
+    ((= index 0) (first list))
   ;; Else, return the nth element of the rest of the list
-	(t (my-nth (rest list) (- index 1)))))
+    (t (my-nth (rest list) (- index 1)))))
 
 (defun my-last (l1)
   "Return the last item in the list"
@@ -34,49 +42,33 @@
   ;; Else, return the last of the rest of the list.
     (t (my-last (rest l1)))))
 
-(defun my-assoc (key l1)
-  "If l1 is a list of key-value pairs,
-   return the pair that has the key"
-   (cond 
-   ;; If the key is in the first key-value pair
-     ((null l1) nil)
-   ;; If the key is equal to the first of the first of l1, 
-   ;; return the first
-     ((equal key (first (first l1)))
-       (first l1))
-     (t (my-assoc key (rest l1)))))
-
 (defun my-make-array (l1)
   "Make an array of the dimensions specified in l1"
   (cond
+  ;; Return 0 if l1 is null
     ((null l1) 0)
+  ;; Return a one-d array of the first item if rest is null
     ((null (rest l1)) (make-oned-array (first l1)))
+  ;; Return a one-d array wrapped in a list if the first of l1 is 1
     ((equal (first l1) 1)
-      (list (my-make-array (rest l1)))
-    )
+      (list (my-make-array (rest l1))))
+  ;; Recurse to construct an array of the specified dimensions.
     (t
-      (cons (my-make-array (rest l1)) (my-make-array (cons (- (first l1) 1) (rest l1))))
-    )
-  )
-)
+      (cons (my-make-array (rest l1)) 
+        (my-make-array (cons (- (first l1) 1) (rest l1)))))))
 
-(defun my-array-dimension (a n)
-  "Return the nth dimension of the array a"
+(defun my-array-dimension (my-array nth-dimension)
+  "Return the nth dimension of the array"
   (cond 
-    ;((null a) -1)
-    ;((<= 0 n) (length a))
-    ; If the index is 0, return the lenth of the array
-    ((equal n 0)
-      (length a)
-    )
+    ;((null my-array) -1)
+    ;; Return nil if nth-dimension is invalid
+    ((< nth-dimension 0) nil)
+    ;; If the index is 0, return the lenth of the array
+    ((equal nth-dimension 0)
+      (length my-array))
     (t
-      ;(first (my-array-dimension a (- n 1)))
-      (my-array-dimension (first a) (- n 1))
-      ;(length (my-nth a ))
-      ;(my-array-dimension a (- n 1))
-    )
-  )
-)
+    ;; Recurse to find the dimension.
+      (my-array-dimension (first my-array) (- nth-dimension 1)))))
 
 (defun my-set-aref (my-array item indexes)
   "Return a copy of my-array in which my-array[i,j] is set to value, i
@@ -110,31 +102,6 @@
   ;; Extract the row using my-nth1 and then extract the right
   ;; column using my-nth into that column.
   (my-nth1 (my-nth1 my-array (first indexes)) (my-second indexes)))
-
-(defun make-n-lists (a1 l1)
-  "Return a list containing a1 lists of l1"
-  (cond
-  ;; If a1 is 0, return nil
-    ((equal 0 a1) nil)
-  ;; If a1 is 1, retun l1
-    ((equal 1 a1) l1)
-  ;; Else, construct a new list of l1 tacked on to the front
-  ;; of itself a1 times.
-    (t
-      (cons l1 (make-n-lists-aux (- a1 1) l1 (list l1))))))
-
-(defun make-n-lists-aux (a1 l1 l2)
-  "Auxiliary function for make-n-lists so that the last element
-   retains the right number of parens."
-  (cond
-  ;; If a1 is 0, retun nil
-    ((equal 0 a1) nil)
-  ;; If is a1 is 1, retun l2
-    ((equal 1 a1) l2)
-    (t
-  ;; Else, return l1 constructed onto l2 a1 times.
-      (cons l1 (make-n-lists-aux (- a1 1) l1 l2)))))
-
 
 (defun make-oned-array (a1)
   "Make a 1d array of with a1 elements"
